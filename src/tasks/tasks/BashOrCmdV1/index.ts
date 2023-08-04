4e03a01da66ca4ffa37611f915d947764b4a8e55
Missy Messa <47990216+missymessa@users.noreply.github.com>
2023-08-04 08:39:07 -0700

    Remove dead code (#75)
    * First pass at cleaning up deprecated tasks
    
    * Removed more packages without being too overzealous
    
    * Added information about deprecated tasks and reminder to investigate package use before updating it

--- src/tasks/tasks/BashOrCmdV1/index.ts
+++ src/tasks/tasks/BashOrCmdV1/index.ts
@@ -1,39 +1,4 @@
-import tl = require('azure-pipelines-task-lib/task');
-import trm = require('azure-pipelines-task-lib/toolrunner');
-
 async function run() {
-    try {
-        let tool: trm.ToolRunner;
-        let agentOS = process.env["AGENT_OS"];
-        if (agentOS === 'Windows_NT') {
-            console.log("Windows... using CMD");
-            let scriptPath = process.env["INPUTCMD"] || null;
-            if (null === scriptPath) {
-                throw "Missing path to CMD script";
-            }
-            let cmdPath = tl.which('cmd');
-            tool = tl.tool(cmdPath).arg('/c').arg(scriptPath);
-        } else if (agentOS === 'Linux' || agentOS === 'Darwin') {
-            console.log("macOS/Linux... using Bash");
-            let scriptPath = process.env["INPUTBASH"] || null;
-            if (null === scriptPath) {
-                throw "Missing path to Bash script";
-            }
-            let bashPath = tl.which('bash');
-            tool = tl.tool(bashPath).arg(scriptPath);
-        } else {
-            throw "Unsupported platform: " + agentOS;
-        }
-
-        let result = await tool.exec();
-        return result;
-    } catch (err) {
-        if (err instanceof Error) {
-            tl.setResult(tl.TaskResult.Failed, err.message);
-        } else {
-            tl.setResult(tl.TaskResult.Failed, "Unknown Error");
-        }
-    }
 }
 
 run();
